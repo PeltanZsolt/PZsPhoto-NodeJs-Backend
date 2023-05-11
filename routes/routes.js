9b2f68da3b8e90d030fa3f54ea0e38fe68318679
Peltan Zsolt <peltan.zsolt@yahoo.com>
2023-05-11 22:16:40 +0200

    error message changed

--- routes/routes.js
+++ routes/routes.js
@@ -16,7 +16,7 @@ router.use("/", visitorsNrRoutes);
 router.use('/', viewsNrRoutes)
 
 router.get("*", (req, res) => {
-	res.status(404).json({ error: "Not found." });
+	res.status(404).json({ error: "Not page found ." });
 });
 
 module.exports = router;
