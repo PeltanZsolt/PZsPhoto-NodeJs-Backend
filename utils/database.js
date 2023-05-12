5a00330b9ae596fa8eb33d8fdc45dccee896eb8e
Peltan Zsolt <peltan.zsolt@yahoo.com>
2023-05-12 17:43:13 +0200

    Attempt 4 of fixing db

--- utils/database.js
+++ utils/database.js
@@ -1,31 +1,23 @@
-// const mysql = require("mysql2/promise");
+const mysql = require("mysql2/promise");
+// const mysql = require("mysql");
 
-// const dbConnect = mysql.createPool({
-// 	host: process.env.DB_HOST,
-// 	user: process.env.DB_USER,
-// 	password: process.env.DB_PASSWORD,
-// 	database: process.env.DB_DATABASE,
-// 	multipleStatements: process.env.DB_MULTIPLE_STATEMENTS,
-// });
-
-// const dbConnect = mysql.createPool({
-// 	host: process.env.JAWSDB_HOST,
-// 	user: process.env.JAWSDB_USER,
-// 	password: process.env.JAWSDB_PASSWORD,
-// 	database: process.env.JAWSDB_DATABASE,
-// 	multipleStatements: process.env.DB_MULTIPLE_STATEMENTS,
-// });
-
-var mysql = require("mysql");
 require("dotenv").config();
-var dbConnect = mysql.createConnection(process.env.JAWSDB_URL);
+var dbConnect;
 
-dbConnect.connect(function (err) {
-	if (err) {
-		console.log("Error ocured while connecting to database", err);
-		throw err;
-	}
-	console.log("Connected!");
+if (process.env.JAWSDB_URL) {
+	dbConnect = mysql.createPool(process.env.JAWSDB_URL);
+} else {
+	dbConnect = mysql.createPool({
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE,
+		multipleStatements: process.env.DB_MULTIPLE_STATEMENTS,
+	});
+}
+
+dbConnect.on("connection", () => {
+	console.log("DB connection established");
 });
 
 module.exports = dbConnect;
