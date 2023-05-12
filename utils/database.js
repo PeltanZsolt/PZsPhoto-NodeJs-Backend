b94cb61dd1c317cf8d4ae05c9a39c4c762d0eaff
Peltan Zsolt <peltan.zsolt@yahoo.com>
2023-05-13 00:24:14 +0200

    Attempt 17 of fixing DB

--- utils/database.js
+++ utils/database.js
@@ -5,16 +5,32 @@ var dbConnect;
 
 if (process.env.JAWSDB_URL) {
 	console.log("JAWSDB_URL found. Initializing database...");
-	dbConnect = mysql.createConnection(process.env.JAWSDB_URL);
-	dbConnect.connect();
+	dbConnect = mysql.createPool({
+		host: process.env.JAWSDB_HOST,
+		user: process.env.JAWSDB_USER,
+		password: process.env.JAWSDB_PASSWORD,
+		database: process.env.JAWSDB_DATABASE,
+		multipleStatements: process.env.DB_MULTIPLE_STATEMENTS,
+	});
 	dbConnect.on("connection", (mes) => {
-		console.log("DB connection established", mes);
+		console.log("JAWSDB connection established", mes);
 	});
 	dbConnect.on("error", (err) => {
 		console.log("JAWSDB connection could not be established", err);
 	});
-	const users = dbConnect.query("SELECT * FROM users");
-    console.log('Users: ', users)
+
+	// if (process.env.JAWSDB_URL) {
+	// 	console.log("JAWSDB_URL found. Initializing database...");
+	// 	dbConnect = mysql.createConnection(process.env.JAWSDB_URL);
+	// 	dbConnect.connect();
+	// 	dbConnect.on("connection", (mes) => {
+	// 		console.log("DB connection established", mes);
+	// 	});
+	// 	dbConnect.on("error", (err) => {
+	// 		console.log("JAWSDB connection could not be established", err);
+	// 	});
+	// 	const users = dbConnect.query("SELECT * FROM users");
+	//     console.log('Users: ', users)
 } else {
 	dbConnect = mysql2.createPool({
 		host: process.env.DB_HOST,
