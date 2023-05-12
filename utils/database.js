069dad6b399ac53ac46c754956e340ad56419f55
Peltan Zsolt <peltan.zsolt@yahoo.com>
2023-05-13 00:14:16 +0200

    Attempt 16 of fixing DB

--- utils/database.js
+++ utils/database.js
@@ -1,21 +1,22 @@
+const mysql = require("mysql");
+const mysql2 = require("mysql2/promise");
 require("dotenv").config();
 var dbConnect;
 
 if (process.env.JAWSDB_URL) {
 	console.log("JAWSDB_URL found. Initializing database...");
-	const mysql = require("mysql");
 	dbConnect = mysql.createConnection(process.env.JAWSDB_URL);
 	dbConnect.connect();
-	dbConnect.on("connection", () => {
-		console.log("DB connection established");
+	dbConnect.on("connection", (mes) => {
+		console.log("DB connection established", mes);
 	});
-	dbConnect.on("error", () => {
-		console.log("JAWSDB connection could not be established");
+	dbConnect.on("error", (err) => {
+		console.log("JAWSDB connection could not be established", err);
 	});
-	dbConnect.query("SELECT * FROM");
+	const users = dbConnect.query("SELECT * FROM users");
+    console.log('Users: ', users)
 } else {
-	const mysql = require("mysql2/promise");
-	dbConnect = mysql.createPool({
+	dbConnect = mysql2.createPool({
 		host: process.env.DB_HOST,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
